0db6d41ae7a85f8b2d50aafe8beb3ee0d6a06b53
Unknow <dan.belissa@edu.ece.fr>
2021-05-24 01:07:12 +0200

    header

---
+++ includes/header.php
@@ -0,0 +1,42 @@
+<header>
+	<div id="container">
+		<nav class="navbar navbar-expand-sm navbar-light">
+			<a class="navbar-brand" href="#">
+				<img src="img/logo3.JPG" alt="ECE MP"  height="50" class="d-inline-block align-top">
+				<span class="hidden"></span>
+			</a>
+			<button
+			class="navbar-toggler ml-auto custom-toggler"
+			type="button"
+			data-bs-toggle="collapse"
+			data-bs-target="#toggleMobileMenu"
+			aria-controls="toggleMobileMenu"
+			aria-expanded="false"
+			aria-label="Toggle navigation"
+			>
+			<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="toggleMobileMenu">
+				
+				<ul class="navbar-nav ms-auto text-center">
+					<li class="nav-item active">
+						<a class="nav-link" href="#">Accueil</a>
+					</li>
+					<li class="nav-item active">
+						<a class="nav-link" href="#">Tout Parcourir</a>
+					</li>
+					<li class="nav-item active">
+						<a class="nav-link" href="#">Notifications</a>
+					</li>
+					<li class="nav-item active">
+						<a class="nav-link" href="#">Panier</a>
+					</li>
+					<li class="nav-item active">
+						<a class="nav-link" href="#">Votre Compte</a>
+					</li>
+				</ul>
+				
+			</div>
+			<link href="css/header.css" rel="stylesheet">
+		</nav>
+	</header>
--- index.php
+++ index.php
@@ -1,13 +1,16 @@
-<?php 
-
-?>
-
 <!DOCTYPE html>
 <head>
 	<meta charset="UTF-8">
 	<title>ECE MarketPlace !</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<script src="jQuery/jquery-3.6.0.min.js"></script>
+	<script src="js/popper.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+
 </head>
-<body>
-	
+<body style="background-color: #007179;">
+<div class="container-fluid">
+<?php require 'includes/header.php'; ?>
+</div>
 </body>
 </html>
